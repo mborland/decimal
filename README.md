5cac6a89d46673e1ee395c743d7911e8d4190168
Matt Borland <matt@mattborland.com>
2022-09-27 12:12:41 -0700

    Project framework

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# decimal
+# Decimal
+
+An implementation of IEEE Decimal Floating Point for inclusion in Boost
+
+See C++ proposal: https://open-std.org/jtc1/sc22/wg21/docs/papers/2014/n3871.html
---
+++ include/boost/decimal/decimal.hpp
@@ -0,0 +1,15 @@
+//  Copyright (c) 2022 Matt Borland
+//  Use, modification and distribution are subject to the
+//  Boost Software License, Version 1.0. (See accompanying file
+//  LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef BOOST_DECIMAL
+#define BOOST_DECIMAL
+
+namespace boost::decimal {
+
+
+
+}
+
+#endif // BOOST_DECIMAL
