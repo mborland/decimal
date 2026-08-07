cf5a83461147b52df2cc5041f0bd7679be5d8ba2
Matt Borland <matt@mattborland.com>
2026-08-07 15:14:37 -0400

    Make LLDB and GDB match NATVIS

--- extra/detail/generate_string.py
+++ extra/detail/generate_string.py
@@ -2,20 +2,17 @@
 # Distributed under the Boost Software License, Version 1.0.
 # https://www.boost.org/LICENSE_1_0.txt
 
+# Mirrors to_chars_cohort_preserving_scientific, including the sign and stored exponent
+# of a zero, both of which are part of the cohort member being displayed.
 def generate_string(significand, exp, sign):
-    if significand == 0:
-        result = "0e+00"
+    sig_str = str(significand)
+    n_digits = len(sig_str)
+
+    if n_digits == 1:
+        normalized_str = sig_str
+        total_exp = exp
     else:
-        sig_str = str(significand)
-        n_digits = len(sig_str)
+        normalized_str = sig_str[0] + '.' + sig_str[1:]
+        total_exp = exp + n_digits - 1
 
-        if n_digits == 1:
-            normalized_str = sig_str
-            total_exp = exp
-        else:
-            normalized_str = sig_str[0] + '.' + sig_str[1:]
-            total_exp = exp + n_digits - 1
-
-        result = f"{'-' if sign else ''}{normalized_str}e{total_exp:+03d}"
-
-    return result
+    return f"{'-' if sign else ''}{normalized_str}e{total_exp:+03d}"
